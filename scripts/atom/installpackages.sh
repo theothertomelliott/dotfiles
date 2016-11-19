e27cca8918d6f96e205db6da526108807984624c
Tom Elliott <tom.w.elliott@gmail.com>
2016-11-19 18:18:26 -0500

    atom: Add go-plus packages and platform-ide-terminal with configuration

--- scripts/atom/installpackages.sh
+++ scripts/atom/installpackages.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+apm install autocomplete-go@1.3.0
+apm install builder-go@1.0.1
+apm install go-config@1.2.4
+apm install go-debug@0.1.22
+apm install go-get@2.1.1
+apm install go-plus@4.3.2
+apm install godoc@1.1.1
+apm install gofmt@1.2.0
+apm install gometalinter-linter@1.2.2
+apm install gorename@1.1.0
+apm install linter@1.11.18
+apm install navigator-go@1.1.3
+apm install platformio-ide-terminal@2.2.2
+apm install tester-go@2.0.2

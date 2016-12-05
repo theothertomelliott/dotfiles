3638dca0475fee076722476a91a256fa74cef1a5
Tom Elliott <telliott@yext.com>
2016-12-05 18:07:11 -0500

    Add yaml linter and git blame

--- scripts/atom/installpackages.sh
+++ scripts/atom/installpackages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apm install autocomplete-go@1.3.0
 apm install builder-go@1.0.1
+apm install git-blame@0.4.11
 apm install go-config@1.2.4
 apm install go-debug@0.1.22
 apm install go-get@2.1.1
@@ -12,6 +13,7 @@ apm install gorename@1.1.0
 apm install jekyll@1.4.3
 apm install language-soy@0.3.2
 apm install linter@1.11.18
+apm install linter-js-yaml@1.2.6
 apm install navigator-go@1.1.3
 apm install platformio-ide-terminal@2.2.2
 apm install tester-go@2.0.2

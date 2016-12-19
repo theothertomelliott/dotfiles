e45675c9b272993d5dfca06915312a3f67590bb1
Tom Elliott <telliott@yext.com>
2016-12-19 16:56:43 -0500

    Upgrade go-plus

--- scripts/atom/installpackages.sh
+++ scripts/atom/installpackages.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-apm install autocomplete-go@1.3.0
-apm install builder-go@1.0.1
 apm install git-blame@0.4.11
-apm install go-config@1.2.4
-apm install go-debug@0.1.22
-apm install go-get@2.1.1
-apm install go-plus@4.3.2
-apm install godoc@1.1.1
-apm install gofmt@1.2.0
-apm install gometalinter-linter@1.2.2
-apm install gorename@1.1.0
+apm install go-debug@1.0.0
+apm install go-plus@5.0.2
+apm install go-signature-statusbar@1.2.0
+apm install hyperclick@0.0.39
 apm install jekyll@1.4.3
 apm install language-soy@0.3.2
 apm install linter@1.11.18
 apm install linter-js-yaml@1.2.6
-apm install navigator-go@1.1.3
 apm install platformio-ide-terminal@2.2.2
-apm install tester-go@2.0.2

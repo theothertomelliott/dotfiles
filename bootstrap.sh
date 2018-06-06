3b225e498873eadf31b651e869339db5e7fb8ada
Tom Elliott <telliott@yext.com>
2018-06-06 17:00:55 -0400

    Install Vundle if required

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ DOTS="$PWD"
 
 # Clean up existing
 rm -rf ~/.vimrc
+[[ -d ~/.vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -sFvh $DOTS/vimrc ~/.vimrc

93041db5ee712ec80e49ad98dbe498d8061faf84
Tom Elliott <telliott@yext.com>
2018-06-06 16:55:46 -0400

    Remove Atom configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,5 @@ DOTS="$PWD"
 
 # Clean up existing
 rm -rf ~/.vimrc
-rm -rf ~/.atom
 
 ln -sFvh $DOTS/vimrc ~/.vimrc
-ln -sFvh $DOTS/atom ~/.atom
-
-$DOTS/scripts/atom/clearpackages.sh
-$DOTS/scripts/atom/installpackages.sh
-
--- scripts/atom/savepackages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo "#!/bin/bash" > $DIR/installpackages.sh
-apm list --installed --bare | xargs -n1 echo "apm install" >> $DIR/installpackages.sh
-chmod +x $DIR/installpackages.sh
